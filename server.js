652c412e4afd01269a9236333a6b164eb2ad188c
SHABEEB-LAPTOP\shabe <shabeebhasan@hotmail.com>
2019-09-18 23:51:35 +0500

    API for questions get

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var express = require('express');
+var app = express();
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -11,3 +13,17 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+app.get('/questions-one', function (req, res) {
+  con.query("SELECT * FROM player_survey_1_questions", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.end( JSON.stringify(result) );
+  });
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
