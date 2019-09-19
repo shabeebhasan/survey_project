ddca1409c149c8349abc47c8a552c52ad2895508
SHABEEB-LAPTOP\shabe <shabeebhasan@hotmail.com>
2019-09-19 14:23:56 +0500

    Bootstrap styling and Welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MDBBootstrapModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  title = 'Welcome to my experiment';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
