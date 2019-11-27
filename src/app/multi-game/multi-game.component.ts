8d0bf9f06165c8b87e5e3a54167764efffc8c3f2
SHABEEB-LAPTOP\shabe <shabeebhasan@hotmail.com>
2019-11-28 00:11:16 +0500

    added leader board too in multi game

--- src/app/multi-game/multi-game.component.ts
+++ src/app/multi-game/multi-game.component.ts
@@ -156,6 +156,16 @@ export class MultiGameComponent implements OnInit {
             this.opacity = (this.tagCount - this.THRESHOLD_2) / (this.THRESHOLD_3 - this.THRESHOLD_2)
             console.log(this.opacity)
         }
+
+        //leaderBoard
+        this.chartDatasets = [
+            {
+                data: [
+                    100, 50, 30, 10, this.tagCount
+                ],
+                label: ''
+            }
+        ];
     }
 
     setVirtualImage() {
@@ -212,4 +222,45 @@ export class MultiGameComponent implements OnInit {
         }
         return a;
     }
+
+    public chartType : string = 'bar';
+
+    public chartDatasets : Array < any > = [
+        {
+            data: [
+                100, 50, 30, 10
+            ],
+            label: ''
+        }
+    ];
+
+    public chartLabels : Array < any > = ['verdandi', 'neo23', 'legolas', 'snork85', 'you'];
+
+    public chartColors : Array < any > = [
+        {
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 2
+        }
+    ];
+
+    public chartOptions : any = {
+        responsive: true
+    };
+    public chartClicked(e : any) : void {}
+    public chartHovered(e : any) : void {}
 }
